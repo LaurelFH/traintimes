b3c1548e1190869d4bc2d770526273e8e14b6803
unknown <footehud@gmail.com>
2017-07-04 23:28:07 -0400

    initial layout and flow of the app

---
+++ javascript.js
@@ -0,0 +1,45 @@
+//all javascript for the traintimes app 
+// USER FLOW
+// 1.  the user comes to the page and can see the input spaces to add their own train info to a schedule; this incldues the name, destination, first time (military time) and the frq.
+// 2.  the user should then click the submit button, and see the information they added pop up on the schedule panel;  the freq should reflect the current time as a base (relative to it)
+//3.  the user should be able to see this info from many different computers  
+// 4.  If they make a mistake, they could potentially delete their information 
+// 
+
+// COMPUTER FLOW
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+// GLOBAL VARIABLES
+// 
+// 
+// 
+// 
+// 
+
+// ALL FUNCTIONS
+
+// 
+// 
+// 
+// 
+
+
+// CALLING THE EVENTS/ACTIONS 
+
+// 
+// 
+// 
+// 
+// 
+
+//TASKS TO BE COMPLETED:
+//1.
+//2.
+//3.
